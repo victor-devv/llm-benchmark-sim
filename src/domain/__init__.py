d0af6646a737d9ac742d8d1f5a9da6d48ac3dac3
Victor Ikuomola <vheecthurikuomola@gmail.com>
2024-10-24 13:05:40 +0100

    chore: cleanup

--- src/domain/__init__.py
+++
@@ -1,9 +0,0 @@
-from entities.llm import LLM
-from entities.metric import Metric
-from entities.benchmark import Benchmark
-
-__all__ = [
-    "LLM",
-    "Metric",
-    "Benchmark",
-]
--- src/infrastructure/database/__init__.py
+++
@@ -1,9 +0,0 @@
-from src.infrastructure.database.base import Base
-from src.infrastructure.utils.config import settings as settings_config
-
-__all__ = [
-    "Base",
-    "settings_config",
-    "LLM",
-    "Metric",
-]
